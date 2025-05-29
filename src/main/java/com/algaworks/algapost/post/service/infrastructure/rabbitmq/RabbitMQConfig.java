b82de9f509428c37dbca4f4c253adfe65a55bbbe
RE045908 <re045908@qintess.com>
2025-05-29 17:53:13 -0300

    Classes of configuration of RabbitMQ

---
+++ src/main/java/com/algaworks/algapost/post/service/infrastructure/rabbitmq/RabbitMQConfig.java
@@ -0,0 +1,33 @@
+package com.algaworks.algapost.post.service.infrastructure.rabbitmq;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String FANOUT_EXCHANGE_POST_PROCESSING = "post-service.post-processing.v1.e";
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter(ObjectMapper objectMapper){
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public FanoutExchange exchange(){
+        return ExchangeBuilder.fanoutExchange(
+                FANOUT_EXCHANGE_POST_PROCESSING
+        ).build();
+    }
+}
