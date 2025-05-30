5bfb6947564cc058b67c47de3550e8343129016b
RE045908 <re045908@qintess.com>
2025-05-30 17:03:54 -0300

    Organized imports

--- src/main/java/com/algaworks/algapost/post/service/api/controller/PostController.java
+++ src/main/java/com/algaworks/algapost/post/service/api/controller/PostController.java
@@ -2,26 +2,20 @@ package com.algaworks.algapost.post.service.api.controller;
 
 import com.algaworks.algapost.post.service.api.model.PostInput;
 import com.algaworks.algapost.post.service.api.model.PostOutput;
-import com.algaworks.algapost.post.service.api.model.PostProcessingRequestMessage;
 import com.algaworks.algapost.post.service.api.model.PostSummaryOutput;
 import com.algaworks.algapost.post.service.domain.service.PostService;
-import com.algaworks.algapost.post.service.infrastructure.rabbitmq.RabbitMQConfig;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.UUID;
-
 @RestController
 @RequestMapping("api/posts")
 @RequiredArgsConstructor
 public class PostController {
-
 
     private final PostService service;
 
