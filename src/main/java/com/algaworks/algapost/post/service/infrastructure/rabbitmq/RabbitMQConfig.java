3caa6f53f669db04da7c9cb488caa5f6397112e3
RE045908 <re045908@qintess.com>
2025-05-30 14:37:08 -0300

    New configuration to new exchange and queue

--- src/main/java/com/algaworks/algapost/post/service/infrastructure/rabbitmq/RabbitMQConfig.java
+++ src/main/java/com/algaworks/algapost/post/service/infrastructure/rabbitmq/RabbitMQConfig.java
@@ -1,8 +1,7 @@
 package com.algaworks.algapost.post.service.infrastructure.rabbitmq;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.amqp.core.ExchangeBuilder;
-import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -13,6 +12,10 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfig {
 
     public static final String FANOUT_EXCHANGE_POST_PROCESSING = "post-service.post-processing.v1.e";
+
+    public static final String QUEUE_POST_PROCESSING = "post-service.post-processing-result.v1.q";
+
+
 
     @Bean
     public Jackson2JsonMessageConverter jackson2JsonMessageConverter(ObjectMapper objectMapper){
@@ -25,9 +28,25 @@ public class RabbitMQConfig {
     }
 
     @Bean
+    public Queue queue(){
+        return QueueBuilder.durable(QUEUE_POST_PROCESSING).build();
+    }
+
+    @Bean
+    public Binding binding(){
+        return BindingBuilder.bind(queue()).to(exchangePostProcessingResult());
+    }
+
+    @Bean
     public FanoutExchange exchange(){
         return ExchangeBuilder.fanoutExchange(
                 FANOUT_EXCHANGE_POST_PROCESSING
+        ).build();
+    }
+
+    public FanoutExchange exchangePostProcessingResult(){
+        return ExchangeBuilder.fanoutExchange(
+                "text-processor-service.post-processing-result.v1.e"
         ).build();
     }
 }
