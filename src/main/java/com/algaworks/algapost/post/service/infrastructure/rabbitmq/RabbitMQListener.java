41c9d1a7f43530233fb354220e760b9bda397f8d
RE045908 <re045908@qintess.com>
2025-05-30 17:12:26 -0300

    Artifacts resulting from the process that calculates post value

---
+++ src/main/java/com/algaworks/algapost/post/service/infrastructure/rabbitmq/RabbitMQListener.java
@@ -0,0 +1,22 @@
+package com.algaworks.algapost.post.service.infrastructure.rabbitmq;
+
+import com.algaworks.algapost.post.service.api.model.PostProcessingResultMessage;
+import com.algaworks.algapost.post.service.domain.service.PostService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RabbitMQListener {
+
+    private final PostService service;
+
+    @RabbitListener(queues = RabbitMQConfig.QUEUE_POST_PROCESSING_RESULT)
+    public void handle(@Payload PostProcessingResultMessage postProcessingResultMessage) {
+        service.updateValuePost(postProcessingResultMessage);
+    }
+}
