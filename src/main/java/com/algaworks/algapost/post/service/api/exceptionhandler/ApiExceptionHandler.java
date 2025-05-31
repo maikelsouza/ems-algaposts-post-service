e5432b40ecbb12b3b73206d2ff626ef003df1d4b
RE045908 <re045908@qintess.com>
2025-05-31 14:09:40 -0300

    Add ApiExceptionHandler.java

---
+++ src/main/java/com/algaworks/algapost/post/service/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.algaworks.algapost.post.service.api.exceptionhandler;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.net.URI;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@RestControllerAdvice
+@Slf4j
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private MessageSource messageSource;
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatusCode status,
+                                                                  WebRequest request) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(status);
+        problemDetail.setTitle("One or more fields are not valid");
+        problemDetail.setType(URI.create("https://algaposts.com/erros/invalid-fields"));
+
+        Map<String, String> fields = ex.getBindingResult().getAllErrors()
+                .stream()
+                .collect(Collectors.toMap(objectError -> ((FieldError) objectError).getField(),
+                        objectError -> messageSource.getMessage(objectError, LocaleContextHolder.getLocale())));
+
+        fields.forEach((field, message) ->
+                log.warn("Field not valid: '{}' - reason: {}", field, message)
+        );
+        problemDetail.setProperty("fields", fields);
+
+        return handleExceptionInternal(ex, problemDetail, headers, status, request);
+    }
+}
